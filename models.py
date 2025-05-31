c9760c3a3744207f4f028021b2bd0159fc476973
joseph <joseph.ojiambo@student.moringaschool.com>
2025-05-31 13:20:12 +0300

    two commit

---
+++ models.py
@@ -0,0 +1,20 @@
+class Workout:
+    """
+    Represents a fitness workout session.
+    """
+    def __init__(self, workout_type, duration, calories, date):
+        self.workout_type = workout_type
+        self.duration = duration  # in minutes
+        self.calories = calories
+        self.date = date  # format: YYYY-MM-DD
+
+    def to_dict(self):
+        """
+        Converts the Workout instance into a dictionary.
+        """
+        return {
+            "workout_type": self.workout_type,
+            "duration": self.duration,
+            "calories": self.calories,
+            "date": self.date
+        }
